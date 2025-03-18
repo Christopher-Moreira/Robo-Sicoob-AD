2ec1199993baeef8349b121024345f454b8d7ae0
Christopher Moreira <moreirachristopher100@gmail.com>
2025-03-18 10:31:02 -0300

    Detectação de Imagens

--- Monitorar.py
+++ Monitorar.py
@@ -1,10 +1,10 @@
 from python_imagesearch.imagesearch import imagesearch
 
 
-imagem_referencia_erro = "image.png"
+imagem_referencia_erro = "Images/Erro_Cobraca_Administrativa_Salvar_Relatorio.png"
 
 # Verificar a tela
-pos = imagesearch(imagem_referencia_erro)  # Procura a imagem de referência
+pos = imagesearch(imagem_referencia_erro)   # Procura a imagem de referência
 if pos[0] != -1:
     print("A tela corresponde à imagem de referência.")
 else:
